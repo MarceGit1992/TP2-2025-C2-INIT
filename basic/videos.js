a38657458735a59454ca8d16fe432ae8351dab91
Pablo Hinojosa <pablo.hinojosa@ort.edu.ar>
2025-08-28 22:52:07 +0000

    + solucion de videos

--- basic/videos.js
+++ basic/videos.js
@@ -32,3 +32,27 @@ const str = `<ul>
 
 const stringNombre = "Juan, Ana, Pedro, Maria, Luis, Carlos";
 const arrayNombre = stringNombre.split(", ");
+
+function getVideos(str) {
+  return str
+          .replace("<ul>","")
+          .replace("</ul>","")
+          .split("</li>")
+          .slice(0, -1)
+          .map(video => ({
+            min: parseInt(video.split('"')[1].split(':')[0]), 
+            seg: parseInt(video.split('"')[1].split(':')[1]), 
+            tipo:video.split('>')[1]
+          }));
+}
+
+function getTotalSegundos(videos, tipo) {
+  return videos
+          .filter(video => video.tipo === tipo)
+          .reduce((total, video) => total + (video.min * 60) + video.seg, 0);
+  
+  // se puede hacer con forEach
+  // tambien se puede hacer con reduce
+}
+
+console.log(getTotalSegundos(getVideos(str), 'Flexbox Video')); // 12717
