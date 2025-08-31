8e9b4ec3d7611f01950b5c618a531e9596301876
MarceGit1992 <marcelo@kubernights.com>
2025-08-31 17:26:50 -0300

    tarea promesas

--- async/promesas.js
+++ async/promesas.js
@@ -1,6 +1,6 @@
-import fs from 'fs/promises';
+import fs from "fs/promises";
 
-const path = './inventors.json';
+const path = "./inventors.json";
 
 // 1.- Leer el archivo inventors.json de forma asyncrona con promesas
 // 2.- TODO Agregar un nuevo inventor
@@ -8,12 +8,25 @@ const path = './inventors.json';
 // 4.- TODO: Eliminar el inventor recien agregado
 // 5.- TODO: Persistir el archivo, sin el inventor recien agregado
 
-fs.readFile(path, 'utf-8')
-    .then(data => {
-        console.log(data);
-    })
-    .catch(err => {
-        console.log("Error leyendo el archivo", err);
-    });
+fs.readFile(path, "utf-8")
+  .then((data) => {
+    const inventors = JSON.parse(data);
+    inventors.push({ first: "Anna", last: "Fernandez", year: 2028 });
+    console.log(inventors);
+    return fs.writeFile(path, JSON.stringify(inventors, null, " "), "utf-8");
+  })
+
+  .then(() => {
+    return fs.readFile(path, "utf-8");
+  })
+  .then((data) => {
+    const inventors = JSON.parse(data);
+    inventors.pop();
+    console.log(inventors);
+    return fs.writeFile(path, JSON.stringify(inventors, null, " "), "utf-8");
+  })
+  .catch((err) => {
+    console.log("Error leyendo el archivo", err);
+  });
 
 console.log("Realizando operaciones ...");
--- async/promesas2.js
+++ async/promesas2.js
@@ -5,26 +5,43 @@
 // 5.- TODO: Persistir el archivo, sin el inventor recien agregado
 // 6.- TODO: Actualizar el inventor recien agregado (cambiar el año a 2020)
 
-import fs from 'fs/promises';
-const path = './inventors.json';
+import fs from "fs/promises";
+const path = "./inventors.json";
 const newInventor = {
-    first: "Anna",
-    last: "Fernandez",
-    year: 2020
+  first: "Anna",
+  last: "Fernandez",
+  year: 2028,
 };
 
 async function getInventors() {
-    return fs.readFile(path, 'utf-8');
+  return fs.readFile(path, "utf-8");
 }
 
 async function addInventor(inventors) {
-    inventors.push(newInventor);
-    fs.writeFile(path, JSON.stringify(inventors, null, " "), 'utf-8');
-    return inventors;
+  inventors.push(newInventor);
+  return fs.writeFile(path, JSON.stringify(inventors, null, " "), "utf-8");
+  //return inventors;
 }
 
-const inventors = JSON.parse(await getInventors()); 
+async function removeInventor(inventors) {
+  inventors.pop();
+  return fs.writeFile(path, JSON.stringify(inventors, null, " "), "utf-8");
+  //return inventors;
+}
 
-const conNuevoInventor = await addInventor(inventors);
+async function editInventor(inventors) {
+  const inventor = inventors[inventors.length - 1];
+  inventor.year = 2030;
+  return fs.writeFile(path, JSON.stringify(inventors, null, " "), "utf-8");
+}
 
-console.log(conNuevoInventor);
+const inventors = JSON.parse(await getInventors());
+
+console.log(inventors);
+
+await addInventor(inventors);
+console.log(inventors);
+await editInventor(inventors);
+console.log(inventors);
+await removeInventor(inventors);
+console.log(inventors);
